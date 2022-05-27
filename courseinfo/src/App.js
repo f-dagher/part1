5e3d981ba92c90a6ccfe8eca7be1109960237b2c
f-dagher <fdagher95@gmail.com>
2022-05-27 00:36:53 -0400

    Updating courseinfo with 1.4

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -20,59 +20,48 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} exer={props.exer1} />
-      <Part part={props.part2} exer={props.exer2} />
-      <Part part={props.part3} exer={props.exer3} />
+      <Part part={props.parts[0].name} exer={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exer={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exer={props.parts[2].exercises} />
     </div>
   );
 };
 // renders the total number of excercises
 const Total = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Number of exercises {props.sum}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </div>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+ 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1.name}
-        exer1={part1.exercises}
-        part2={part2.name}
-        exer2={part2.exercises}
-        part3={part3.name}
-        exer3={part3.exercises}
-      />
-      <Total sum={part1.exercises + part2.exercises + part2.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
 
 export default App;
 
-/**
- *   const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
- */
+
